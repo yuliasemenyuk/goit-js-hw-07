1b5af400e4677432a017086df2cbc444704aa194
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-17 21:30:35 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryItemMarkup = galleryItems.map((item) => 
+`<li class="gallery__item">
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>`).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
+
+
